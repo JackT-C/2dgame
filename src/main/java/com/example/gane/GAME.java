df7e7c4a42902af20d3b5c5a957d63b0409018f0
JackT-C <jt210471@reigate.ac.uk>
2022-03-11 00:17:38 +0000

    game

--- src/main/java/com/example/gane/GAME.java
+++ src/main/java/com/example/gane/GAME.java
@@ -1,4 +1,27 @@
 package com.example.gane;
 
-public class GAME {
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class GAME extends Application {
+    public AnchorPane scene;
+    @FXML
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(LOADINGSCREEN.class.getResource("GAME.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 750, 420);
+        stage.setTitle("GAME");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+
 }
--- src/main/java/com/example/gane/LOADINGSCREEN.java
+++ src/main/java/com/example/gane/LOADINGSCREEN.java
@@ -2,34 +2,55 @@ package com.example.gane;
 
 import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+
+import java.io.IOException;
 
 
 public class LOADINGSCREEN extends Application {
     public ImageView placeholder;
-
-    public void mousepress() {
-
-    }
+    private AnchorPane scene;
+    @FXML
+    private AnchorPane rootPane;
 
     public void mouserelease() {
         FadeTransition ft = new FadeTransition(Duration.millis(1000),placeholder);
         ft.setFromValue(1.0);
         ft.setToValue(0.3);
-        ft.setCycleCount(4);
         ft.setAutoReverse(true);
         ft.play();
+        ft.setOnFinished(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try {
+                    switchscene();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+    @FXML
+    public void switchscene() throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("GAME.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(LOADINGSCREEN.class.getResource("LOADING SCREEN.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 750, 420);
-        stage.setTitle("Loading Screen");
+        Parent root = FXMLLoader.load(getClass().getResource("LOADING SCREEN.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("game");
         stage.setScene(scene);
         stage.show();
     }
